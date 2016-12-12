692ad9001c73b8e309ae9dbd93b18542858c3a77
Damián <zarrastro@gmail.com>
2016-12-12 15:38:09 +0100

    Fixes y musiquetas Necesito aprender a desbloquear niveles

---
+++ Assets/scripts/unlockNextLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unlockNextLevel : MonoBehaviour {
+
+    public string nextLevel;
+
+	void OnCollisionEnter (Collision col) {
+        if (col.gameObject.layer == 8 || col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            
+        }
+    }
+}
